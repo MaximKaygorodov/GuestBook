64352b220936d499fd8ba94a618a202593e0c77a
MaximKaygorodov <masimus.goto@gmail.com>
2020-10-03 19:22:21 +0300

    Updates

---
+++ precache-manifest.1e040b1f05c931173e7fcc69c20143c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d8978311f3f9a473b079eaa8a00cf9",
+    "url": "/GuestBook/index.html"
+  },
+  {
+    "revision": "66effdfb7011a93b9384",
+    "url": "/GuestBook/static/js/2.8bd65d9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GuestBook/static/js/2.8bd65d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b89e361cd8f000cdfc9",
+    "url": "/GuestBook/static/js/main.1be12123.chunk.js"
+  },
+  {
+    "revision": "8dd6b0492e519e951e00",
+    "url": "/GuestBook/static/js/runtime-main.80bd59ad.js"
+  }
+]);
