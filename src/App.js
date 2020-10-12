af7e08255dd0d8459e7cc95261c443a489e84ae4
MaximKaygorodov <masimus.goto@gmail.com>
2020-10-12 18:30:29 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@ class App extends Component {
 
   getContent = () => {
     
-    const url = "https://servername123.herokuapp.com/api/submit?name=${encodeURIComponent(this.state.name)}&message=${encodeURIComponent(this.state.message)}";
-    alert(url)
+    const url = `https://servername123.herokuapp.com/api/submit?name=${encodeURIComponent(this.state.name)}&message=${encodeURIComponent(this.state.message)}`;
+    // alert(url)
     fetch(url)
       .then(response => response.json())
       .then(state => this.setState({book : state}));
